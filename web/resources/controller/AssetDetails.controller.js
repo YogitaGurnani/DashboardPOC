960381bb9afe3756ab194524b1bd139ad9f40b32
Yogita Gurnani <yogitagurnani30@gmail.com>
2019-07-24 11:02:10 +0000

    api changes

--- web/resources/controller/AssetDetails.controller.js
+++ web/resources/controller/AssetDetails.controller.js
@@ -4,6 +4,7 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("pocNameSpace.poc.controller.AssetDetails", {
+		THINGS_ID : "83E5449C94464D86B7E456B80C7819B2",
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
@@ -11,9 +12,36 @@ sap.ui.define([
 		 * @memberOf pocNameSpace.poc.view.AssetDetails
 		 */
 		onInit: function () {
-
+			this._oComponent = sap.ui.component(sap.ui.core.Component.getOwnerIdFor(this.getView()));
+			this._oRouter = this._oComponent.getRouter();
+			this._oResourceBundle = this._oComponent.getModel("i18n").getResourceBundle();
+			this._oRouter.attachRoutePatternMatched(this._onRoutePatternMatched, this);
+			this.getAssetDetails();
+		
 		},
-
+		
+		getAssetDetails: function(){
+			var sUrl =this._oComponent.getMetadata().getConfig().serviceConfig.getTimeSeriesCall.url.replace("<thingID>", this.THINGS_ID);
+			jQuery.ajax({
+				method: "GET",
+				url: sUrl,
+				dataType: "json",
+				success: function (oResponse) {
+					if(!this.getView().getModel("assetDetails")){
+						var oModel = new sap.ui.model.json.JSONModel();
+						/*var oData  = {
+							firstName : oResponse.firstName || "",
+							lastName : oResponse.lastName || ""
+						};*/
+						oModel.setData(oResponse);
+						this.getView().setModel(oModel,"Details");
+					}
+				}.bind(this),
+				error: function () {
+					this.LOGGER.error("Failure of service call to get user details enumerations");
+				}.bind(this)
+			});
+		}
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
